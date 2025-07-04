0e4014e77f541b77d265dc42ae88c0c68062de79
GameShopCorpGlobal <gameshopengine@gmail.com>
2025-07-04 13:23:29 -0400

    NiftyGUI Added.  Making UI for GameShop Engine.  Doing 3D Modeler next

--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -3,9 +3,12 @@ package com.mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.mygame.niftygui.StartScreenController;
+import de.lessvoid.nifty.Nifty;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -14,6 +17,8 @@ import com.jme3.scene.shape.Box;
  */
 public class Main extends SimpleApplication {
 
+    private Nifty nifty;
+    
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
@@ -29,6 +34,23 @@ public class Main extends SimpleApplication {
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
+        
+        NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                assetManager,
+                inputManager,
+                audioRenderer,
+                guiViewPort);
+        nifty = niftyDisplay.getNifty();
+        StartScreenController startScreen = new StartScreenController(this);
+        nifty.fromXml("Interface/Nifty/HelloGameShop.xml", "start", startScreen);
+
+        // attach the nifty display to the gui view port as a processor
+        guiViewPort.addProcessor(niftyDisplay);
+
+        // disable the fly cam
+        flyCam.setEnabled(false);
+        flyCam.setDragToRotate(true);
+        inputManager.setCursorVisible(true);
     }
 
     @Override
