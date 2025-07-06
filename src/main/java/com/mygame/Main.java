0c518bbf60c62a1d9e727448e39f2a2ac3590fd6
GameShopCorpGlobal <gameshopengine@gmail.com>
2025-07-06 01:16:34 -0400

    Making 3D Modeler
    Making 3D Modeler

--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -3,10 +3,14 @@ package com.mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector4f;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Quad;
+import com.jme3.texture.Texture2D;
+import com.mygame.graphics.ATMS;
 import com.mygame.niftygui.StartScreenController;
 import de.lessvoid.nifty.Nifty;
 
@@ -26,11 +30,16 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
+        reportMemory();
+        Quad b = new Quad(4, 4);
         Geometry geom = new Geometry("Box", b);
+        
+        ATMS atms = new ATMS("Box", 128,128);
+        atms.layer.drawCircle(64, 64, 64, new Vector4f(255,255,255,255));
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
+        mat.setColor("Color", ColorRGBA.White);
+        mat.setTexture("ColorMap", new Texture2D(atms.makeATMS()));
         geom.setMaterial(mat);
 
         rootNode.attachChild(geom);
@@ -41,6 +50,8 @@ public class Main extends SimpleApplication {
                 audioRenderer,
                 guiViewPort);
         nifty = niftyDisplay.getNifty();
+        nifty.loadStyleFile("nifty-default-styles.xml");
+        nifty.loadControlFile("nifty-default-controls.xml");
         StartScreenController startScreen = new StartScreenController(this);
         nifty.fromXml("Interface/Nifty/HelloGameShop.xml", "start", startScreen);
 
@@ -62,5 +73,16 @@ public class Main extends SimpleApplication {
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
     }
+    
+        public void reportMemory(){
+        
+                long maxMemory = Runtime.getRuntime().maxMemory();
+                long allocatedMemory = Runtime.getRuntime().totalMemory();
+                long freeMemory = Runtime.getRuntime().freeMemory();
+                
+                System.out.println("max: " + maxMemory);
+                System.out.println("allocated: " + allocatedMemory);
+                System.out.println("free: " + freeMemory);
+        }
 }
 
