456b2e217b600361b6e0cf8d995c37b392339900
GameShopCorpGlobal <gameshopengine@gmail.com>
2025-07-09 10:05:48 -0400

    Moving On To GameShop Engine 2

--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -11,10 +11,13 @@ import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.math.Vector4f;
 import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
+import com.jme3.scene.CameraNode;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.control.CameraControl.ControlDirection;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
 import com.jme3.texture.Texture2D;
@@ -31,7 +34,7 @@ import de.lessvoid.nifty.Nifty;
  * Move your Logic into AppStates or Controls
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
+public class Main extends SimpleApplication implements ActionListener {
 
     private Nifty nifty;
     
@@ -65,14 +68,22 @@ public class Main extends SimpleApplication {
         guiViewPort.addProcessor(niftyDisplay);
         
         // disable the fly cam
-        flyCam.setEnabled(true);
+        flyCam.setEnabled(false);
         //flyCam.setDragToRotate(true);
-        inputManager.setCursorVisible(true);
+        inputManager.setCursorVisible(false);
+         setUpKeys();
          
         Selector selector = new Selector(base.superMesh, base.superMesh.superMesh.get("front"), 0,0);
-        
+          camNode = new CameraNode("CameraNode", cam);
+            camNode.setControlDir(ControlDirection.SpatialToCamera);
+//Attach the camNode to the target:
+base.superMesh.node.attachChild(camNode);
+//Move camNode, e.g. behind and above the target:
+camNode.setLocalTranslation(new Vector3f(0, 5, -5));
+//Rotate the camNode to look at the target:
+camNode.lookAt(base.superMesh.node.getLocalTranslation(), Vector3f.UNIT_Y);
     }
-
+CameraNode camNode;
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
@@ -97,34 +108,39 @@ public class Main extends SimpleApplication {
         private void setUpKeys() {
     inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
     inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-    inputManager.addListener(analogListener, "Left");
-    inputManager.addListener(analogListener, "Right");
-    inputManager.addListener(analogListener, "Forward");
-    inputManager.addListener(analogListener, "Backward");
+    inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_S));
+//    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+    inputManager.addListener(this, "Left");
+    inputManager.addListener(this, "Right");
+    inputManager.addListener(this, "Forward");
+    inputManager.addListener(this, "Backward");
      
 }
         
-           private final ActionListener actionListener = new ActionListener() {
+           //private final ActionListener actionListener = new ActionListener() {
         @Override
         public void onAction(String name, boolean keyPressed, float tpf) {
-//            if (name.equals("Pause") && !keyPressed) {
-//                isRunning = !isRunning;
-//            }
+ 
+           System.out.println("forward");
+            if (name.equals("Forward") && keyPressed){
+            
+                
+                //System.out.println(tpf);
+            }
         }
-    };
+    //};
 
     private final AnalogListener analogListener = new AnalogListener() {
         @Override
         public void onAnalog(String name, float value, float tpf) {
-            if (name.equals("Forward")){
-            
-            
-               // cam.get
-            }
+             
         }
     };
+
+//    @Override
+//    public void onAction(String string, boolean bln, float f) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
 }
 
